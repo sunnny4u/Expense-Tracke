83c8ed9705de990a552f8c5d55257ff035f457a0
sunnny4u <sunnycse1995@gmail.com>
2019-11-08 08:16:17 +0600

    Home page & Contact page done

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,5 @@ from tracker import views
 
 urlpatterns = [
     path('', views.home_page, name="home_page"),
+    path('contact/', views.contact_page, name="contact_page"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return  render(request, 'home.html')
+
+def contact_page(request):
+    return render(request, 'contact.html')
